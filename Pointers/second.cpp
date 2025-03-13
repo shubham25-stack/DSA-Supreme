42778f2b07d0688a9ad9a018cddc81c53618371e
shubham25-stack <kumarshubham0683@gmail.com>
2025-03-13 23:48:10 +0530

    pointer

--- Pointers/second.cpp
+++ Pointers/second.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-h
+int main() {
+    int a = 100;
+    int *ptr = &a;
+
+    cout << a << " " << &a << " " << *ptr << " " << ptr << << " " << &ptr << " " 
+         << (*ptr)++ << " " << ++(*ptr) << " " << *ptr = *ptr / 2 << " " << *ptr = *ptr - 2 << endl;
+
     return 0;
 }
+
+    // int a = 100;
+    // int *ptr = &a;
+    // a = a+1;
+    // *ptr = *ptr+1;
+    // cout<<a<<endl;
+    // cout<<ptr<<endl;
+
+     
